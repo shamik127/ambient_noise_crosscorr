25a126ac0ed1ea3ca6784040582f905589533630
shamik127 <shamik127@gmail.com>
2020-06-23 15:57:42 +0530

    Changed the python class structure. Added a few more methods

--- geo_plot.py
+++ geo_plot.py
@@ -1,146 +1,154 @@
 #!/usr/bin/python
 
-import os
+from os import path, makedirs
 import sys
 import numpy as np
-import pandas as pd
+from collections import OrderedDict
 import obspy.core as oc
-import scipy.signal as ss
-import matplotlib.pyplot as plt
-import matplotlib.cm as cm
-from matplotlib.pyplot import figure, show, rc
-from mpl_toolkits.basemap import Basemap
+from pandas import read_csv
+#import scipy.signal as ss
+#import matplotlib.cm as cm
+#import matplotlib.pyplot as plt
+#from matplotlib.pyplot import figure, show, rc
 
 
 def snr(dt, cclags, ccmat, umin, umax, rel_recs):
 
     offsets = map(lambda x: x.stats.distance*1e-3, ccmat.traces)
-    
     rsd = np.array(offsets)
-    #print(rsd)
-    kernel = gaussian_kernel(rsd)
-    #print(kernel)
-
-
+    kernel = gaussian_kernel(rsd)    
     tstart = rsd/umax
     tend = rsd/umin
-
     posbr_tl = cclags[len(cclags)//2+1:]
-    
     pb_iws = np.searchsorted(posbr_tl,tstart)
     pb_iwe = np.searchsorted(posbr_tl,tend)
-
     egy_win_pb = np.empty(len(ccmat.traces))
     egy_owin_pb = np.empty(len(ccmat.traces))
 
+    snrall={}
+
     for tn in range(len(ccmat.traces)):
         posbr_cc = ccmat.traces[tn].data
-        #print(posbr_cc[0:50])
         cccopy = posbr_cc.copy()
         pbwin = posbr_cc[pb_iws[tn]:pb_iwe[tn]+1]
-
         egy_win_pb[tn] = np.mean(np.square(pbwin))
         cccopy[pb_iws[tn]:pb_iwe[tn]+1] = 0.0
         egy_owin_pb[tn] = np.sum(np.square(cccopy))/( len(cccopy) - len(pbwin) )
+        #snrall[rel_recs[tn]] = egy_win_pb[tn]/egy_owin_pb[tn] * rsd[tn]/30 * kernel[tn]
+        snrall[rel_recs[tn]] = egy_win_pb[tn]/egy_owin_pb[tn]
 
-        snr_all[rec+1][ang][rel_recs[tn]] = egy_win_pb[tn]/egy_owin_pb[tn] * rsd[tn]/30 * kernel[tn]
-
-    
-    snr_arr = egy_win_pb/egy_owin_pb
-    norm_snr = snr_arr * rsd * kernel
-
-    #print(egy_win_pb/egy_owin_pb)
-    return np.mean(norm_snr)
+    return snrall
 
 
 class do_one_brec:
-    def __init__(self,ccmat,rlist,azs,dist,angles):
+    def __init__(self,ccmat,rlist,azms,azbin):
         a = 2
-        self.azbin = azs
-        self.distances = dist
-        self.angles = angles
+        self.azms=azms
+        self.azbin = azbin
         self.rlist = rlist
         self.ccm = ccmat
-        self.nrec = len(rlist)
+        self.nrec = len(reclist)
 
         self.cum_arr = np.cumsum(np.arange( (self.nrec-1), 0, -1))
         self.cum_arr = np.insert(self.cum_arr, 0, 0)
         self.cum_arr = np.insert(self.cum_arr, -1, self.cum_arr[-1])
 
+        self.master_snr=np.empty([len(self.rlist), len(self.azms)])
+        self.snrall={}
+        self.avgsnrdict={}
+        self.avgsnr=np.empty([len(self.rlist), len(self.azms)])
+        self.get_snr()
 
-    def get_rec_coord(self, rec):
-        x = crd[crd[:,0] == rec, 2]
-        y = crd[crd[:,0] == rec, 3] 
-        return x,y
 
-    def getsection_with_azm(self,brec,azm):
-        try:
-            bri = list(self.rlist).index(brec)
-        except ValueError:
-            sys.exit('Could not find the base receiver.')
+    def get_snr(self):
+        print('\nCalculating snr...')
+        for i,brec in enumerate(self.rlist):
+            #print(brec)
+            try:
+                bri = list(reclist).index(brec)
+            except ValueError:
+                sys.exit('Could not find the base receiver.')
 
-        upperRecs = np.arange(self.cum_arr[bri], self.cum_arr[bri+1])
+            self.snrall[brec]={}
 
-        # print(upperRecs)
+            upperRecs = np.arange(self.cum_arr[bri], self.cum_arr[bri+1])
+            lowerRecs = np.array([], dtype=np.int32)
+            for i in range(0,bri):
+                lowerRecs = np.insert(lowerRecs,0, (self.cum_arr[bri-(i+1)]+i))
+            upperAngs = angles[upperRecs]            
+            lowerAngs = angles[lowerRecs] + 180
+
+            for i in range(len(lowerAngs)):
+                if (lowerAngs[i] > 360):
+                    lowerAngs[i] = lowerAngs[i] - 360
+            #print(azms)
+            for j,azm in enumerate(self.azms):
+                upperAngIdsWithinBin = np.argwhere(np.abs(upperAngs-azm) < self.azbin/2.0).flatten()
+                lowerAngIdsWithinBin = np.argwhere(np.abs(lowerAngs-azm) < self.azbin/2.0).flatten()
+
+                upperRecsIdsWithinBin = upperRecs[upperAngIdsWithinBin]
+                lowerRecsIdsWithinBin = lowerRecs[lowerAngIdsWithinBin]
+
+                #print(lowerAngIdsWithinBin + 1)
+                #print(upperAngIdsWithinBin + (bri+2))
+                rel_recs = np.append( (lowerAngIdsWithinBin + 1), (upperAngIdsWithinBin + (bri+2)) )
+                #print(rel_recs)
+                # print(rel_recs)
+                #print('--------------------')
+                
+                ccml = self.ccm.shape[0]
+                upperTrs = [oc.trace.Trace(data=self.ccm[ccml//2+1:,x], header={'distance': distances[x]}) for x in upperRecsIdsWithinBin]
+                lowerTrs = [oc.trace.Trace(data=np.flip(self.ccm)[ccml//2+1:,x], header={'distance': distances[x]}) for x in lowerRecsIdsWithinBin]
+                final_pnsep= oc.stream.Stream(traces=lowerTrs) + oc.stream.Stream(traces=upperTrs)
+            
+                snrall = snr(si,cclags,final_pnsep,1.5,6.0, rel_recs)
+                self.snrall[brec][azm]=OrderedDict(sorted(snrall.items()))
+            
+        self.avg_snr()
+        print('\nDone!')
+
+    def get_all_snr(self):
+        return self.snrall
+
+
+    def avg_snr(self):
+        for i,rec in enumerate(self.rlist):
+            self.avgsnrdict[rec]={}
+            for j,azm in enumerate(self.azms):
+                avgsnrval=np.mean(self.snrall[rec][azm].values())
+                self.avgsnrdict[rec][azm]=avgsnrval
+                self.avgsnr[i][j]=avgsnrval
         
-        lowerRecs = np.array([], dtype=np.int32)
-        for i in range(0,bri):
-            lowerRecs = np.insert(lowerRecs,0, (self.cum_arr[bri-(i+1)]+i))
 
-        #print(lowerRecs)
-        
-        # Base receiver angles with the upper receivers
-        upperAngs = self.angles[upperRecs]
-        #upperAngs = upperAngs.astype('int32')
-        #print(upperAngs)
+    def get_avgsnrdict(self):
+        return self.avgsnrdict
 
-        # Base receiver angles with the lower receivers (the angles data contains angles corresponding to 
-        # lower to upper receiver)
-        
-        lowerAngs = self.angles[lowerRecs] + 180
-        #lowerAngs = lowerAngs.astype('int32')
-        #print(lowerAngs)
 
-        for i in range(len(lowerAngs)):
-            if (lowerAngs[i] > 360):
-                lowerAngs[i] = lowerAngs[i] - 360
+    def get_avgsnr(self):
+        return self.avgsnr
 
-        #print(lowerAngs)
 
-        # index positions in the angs array needed for the given angle
-        upperAngIdsWithinBin = np.argwhere(np.abs(upperAngs-azm) < self.azbin/2.0).flatten()
-        lowerAngIdsWithinBin = np.argwhere(np.abs(lowerAngs-azm) < self.azbin/2.0).flatten()
+    def save_files(self):
+        np.savetxt('%s/avgsnr.txt' %(name), self.avgsnr, delimiter=',')
+        np.save('%s/avgsnrdict.npy' %(name), self.avgsnrdict)
 
-        upperRecsIdsWithinBin = upperRecs[upperAngIdsWithinBin]
-        lowerRecsIdsWithinBin = lowerRecs[lowerAngIdsWithinBin]
 
-        #print(lowerAngIdsWithinBin + 1)
-        #print(upperAngIdsWithinBin + (bri+2))
-        rel_recs = np.append( (lowerAngIdsWithinBin + 1), (upperAngIdsWithinBin + (bri+2)) )
-        # print(rel_recs)
-        #print('--------------------')
-        
-        ccml = self.ccm.shape[0]
-        upperTrs = [oc.trace.Trace(data=self.ccm[ccml//2+1:,x], header={'distance': self.distances[x]}) for x in upperRecsIdsWithinBin]
-        lowerTrs = [oc.trace.Trace(data=np.flip(self.ccm)[ccml//2+1:,x], header={'distance': self.distances[x]}) for x in lowerRecsIdsWithinBin]
 
-        final_pnsep= oc.stream.Stream(traces=lowerTrs) + oc.stream.Stream(traces=upperTrs)
-        #print(len(final_pnsep.traces))
-        egy = snr(si,cclags,final_pnsep,1.5,6.0, rel_recs)
-        
-        return egy
-
+########################################################################
+# Helper functions
+########################################################################
+def gaussian_kernel(x):
+    kernel = np.exp(-0.5 * (x**2) / np.square(10))
+    return kernel
 
 def stn_pair_distances(rlist):
-    
-
+    crd = read_csv('coordinates_receivers_recnum.csv', sep='\t', header=None)
+    crd.columns = ['rec', 'lat', 'x', 'y']
     nrecs = len(rlist)
-
     distances = np.array([])
     angles = np.array([])
 
     for bid in range(0, nrecs-1):
-    
         base_x = crd.loc[bid].x
         base_y = crd.loc[bid].y
 
@@ -149,7 +157,6 @@ def stn_pair_distances(rlist):
         for rec in recs:
             rec_x = crd.loc[rec].x
             rec_y = crd.loc[rec].y
-
             dist = np.sqrt( (base_x - rec_x)**2 + (base_y - rec_y)**2 )
             if (angVert):
                 qang = np.arctan2(rec_x - base_x, rec_y - base_y)
@@ -160,89 +167,52 @@ def stn_pair_distances(rlist):
                 qang += 2*np.pi
 
             rpazim_d = qang*180/np.pi
-    
             distances = np.append(distances, dist)
             angles = np.append(angles, rpazim_d)
 
-    np.save('distances_99_without.npy', distances)
-    np.save('angles_99_without.npy', angles)
+    np.save('%s/distances.npy' %(name), distances)
+    np.save('%s/angles.npy' %(name), angles)
 
-    return distances, angles
-
-
-def recpair_angles(rlist):
-    angles = {}
-
-    nrecs = len(rlist)
-
-    for bid in range(0, nrecs-1):
-        angles[bid+1] = {}
-        base_x = crd.loc[bid].x
-        base_y = crd.loc[bid].y
-
-        recs = np.arange(0, nrecs ) 
-        recs = np.delete(recs, bid)
-
-        for rec in recs:
-            rec_x = crd.loc[rec].x
-            rec_y = crd.loc[rec].y
-
-            qang = np.arctan2(rec_y - base_y, rec_x - base_x)
-            
-            if (qang < 0.0):
-                qang += 2*np.pi
-
-            rpazim_d = qang*180/np.pi
-            angles[bid+1][rec+1] = rpazim_d
-
-    return angles
-
-def recpair_distances(rlist):
-    distances = {}
-
-    nrecs = len(rlist)
-
-    for bid in range(0, nrecs-1):
-        distances[bid+1] = {}
-        base_x = crd.loc[bid].x
-        base_y = crd.loc[bid].y
-
-        recs = np.arange(0, nrecs ) 
-        recs = np.delete(recs, bid)
-
-        for rec in recs:
-            rec_x = crd.loc[rec].x
-            rec_y = crd.loc[rec].y
-
-            dist = np.sqrt( (base_x - rec_x)**2 + (base_y - rec_y)**2 )
-            distances[bid+1][rec+1] = dist
-
-    return distances
+    #return distances, angles
 
 
-
-def gaussian_kernel(x):
-    kernel = np.exp(-0.5 * (x**2) / np.square(10))
-    return kernel
-
-
+########################################################################
+# Code execution
+########################################################################
 if __name__ == '__main__':
 
+    rewrite = False
+    angVert = True
+
     nargs=len(sys.argv)
-    rec_input=raw_input("Enter a single receiver, or two receivers")
+    print("\n-----------------------------------------------------------------------------")
+    rec_input=raw_input("\nEnter receiver numbers (leave empty to include all receivers): ")
 
-    crd = pd.read_csv('coordinates_receivers_recnum.csv', sep='\t', header=None)
-    crd.columns = ['rec', 'lat', 'x', 'y']
+    # Azimuth bin
+    print("\n-----------------------------------------------------------------------------")
+    azbin=raw_input("\nDesired angle bin width (default=10): ")
+    azbin = int(azbin) if azbin else 10
+    # Desired receiver angles
 
-    angs = np.arange(5, 360, 10)
+    print("\n-----------------------------------------------------------------------------")
+    rec_angs=raw_input("\nEnter the desired angle (leave empty to include all angles): ")
+    if len(rec_angs.split())>0:
+        azms=np.array(rec_angs.split(), dtype=np.int16)
+        if np.sum(np.array([int(azm)%5 for azm in azms])) != 0:
+            sys.exit("One or more invalid angle(s). Angles should multiple of %d" %(azbin//2))
+    else:
+        azms = np.arange(azbin//2, 360, azbin)
 
+    
 
     for fn in range(1,nargs):
-        if not os.path.isfile(sys.argv[fn]):
+        if not path.isfile(sys.argv[fn]):
             sys.exit("File not found: %s" %(sys.argv[fn]))
-        print "Reading ", sys.argv[fn]
+        print "\nReading ", sys.argv[fn]
 
         name=sys.argv[fn][0:-4]
+        if not path.exists(name):
+            makedirs(name)
 
         loaded = np.load(sys.argv[fn])
         reclist=loaded['reclist']
@@ -250,50 +220,33 @@ if __name__ == '__main__':
         cclags=loaded['cclags']
         cookie=loaded['cookie']
         cclags=np.array(map(lambda x: float("%.2f" %x), cclags))
+        print sys.argv[fn], "reading done!"
 
+        if len(rec_input.split())>0:
+            rlist=np.array(rec_input.split(), dtype=np.int16)
+        else:
+            rlist=reclist
+        
         del loaded
-
-        rewrite = True
-        angVert = False
-
         if (rewrite):
             stn_pair_distances(reclist)
 
-        angles = np.load('angles_%s.npy' %(name))
-        distances = np.load('distances_%s.npy' %(name))
+        print "\nReading angles and distances.."
+        angles=np.load('%s/angles.npy' %(name))
+        distances=np.load('%s/distances.npy' %(name))
+        print 'Done!\n'
 
+        #########################################################################################
+        # Result
+        #########################################################################################
+        print("\n##############################################################################")
+        print('Results for file %s.npz' %(name))
+        print("##############################################################################\n")
+
+        rso = do_one_brec(cookie,rlist,azms,azbin)
+        rso.save_files()
+
+        #print(rso.get_avgsnr())
         
-        egy = do_one_brec(cookie,reclist,10,distances, angles)
-
-        master_snr = np.empty([len(reclist), len(angs)])
-
-        snr_all = {}
-        for rec in range(len(reclist)):
-
-            snr_all[rec+1] = {}
-            for j,ang in enumerate(angs):
-                
-                snr_all[rec+1][ang] = {}
-                avg_snr = egy.getsection_with_azm(rec+1, ang)
-                master_snr[rec][j] = avg_snr
-
-        np.savetxt('snr_%s.txt' %(name), master_snr, delimiter=',')
-        #np.save('snr_all_99_without.npy', snr_all)  
-
-        # egy.getsection_with_azm(288, 5)
-
-
-        #print(angs[5])
-
-        # rec_dir = {}
-        # for rec in range(len(reclist)):
-        #     rec_dir[rec+1] = {}
-        #     for ang in angs:
-        #         rec_dir[rec+1][ang] = egy.getsection_with_azm(rec+1, ang)
-
-
-        # np.save('rec_rel.npy', rec_dir)
-        #print(rec_dir)
-        #egy.test_azm(3, 15)
-
-        #print(master_snr)
+        
+        #np.savetxt('snr_%s.txt' %(name), master_snr, delimiter=',')
--- rec_coords.R
+++ rec_coords.R
@@ -4,8 +4,8 @@ library(dplyr)
 
 rec_num = 150
 
-coordinates_rec_latlon_id_gcy <- read_delim("Documents/Arjun/coordinates_rec_latlon_id_gcy.csv", "\t", escape_double = FALSE, col_names = FALSE, trim_ws = TRUE)
-coordinates_receivers_recnum <- read_delim("Documents/Arjun/coordinates_receivers_recnum.csv","\t", escape_double = FALSE, col_names = FALSE,col_types = cols(X1 = col_skip()), trim_ws = TRUE)
+coordinates_rec_latlon_id_gcy <- read_delim("../coordinates_rec_latlon_id_gcy.csv", "\t", escape_double = FALSE, col_names = FALSE, trim_ws = TRUE)
+coordinates_receivers_recnum <- read_delim("../coordinates_receivers_recnum.csv","\t", escape_double = FALSE, col_names = FALSE,col_types = cols(X1 = col_skip()), trim_ws = TRUE)
 
 coordinates_rec_latlon_id_gcy <- data.table(coordinates_rec_latlon_id_gcy)
 coordinates_receivers_recnum <- data.table(coordinates_receivers_recnum)
@@ -20,6 +20,7 @@ x <- seq(0,360,10)
 Result <- left_join(coordinates_receivers_recnum,coordinates_rec_latlon_id_gcy)
 
 # select n random samples 
+#load('recs.RData')
 rn = sample(seq(1,289),rec_num)
 Result = Result[rn,]
 
@@ -27,23 +28,23 @@ result <- Result[rep(seq_len(nrow(Result)), each = length(x)-1), ]
 result
 
 # Dummy SNR data
-
-y <- read_csv("Documents/Arjun/snr_data_100.txt", col_names = FALSE)
-y = y[rn,]
-
-#y <- replicate(length(x)-1, runif(rec_num))
-
-snr_list <- as.vector(t(y))
-length(snr_list)
-result$snr = snr_list
-result$radius = 0.7
-result <- within(result, radius[is.nan(snr)] <- 0.0)
-result <- within(result, snr[is.nan(snr)] <- 0.0)
-#result <- within(result, snr[snr < 1.0] <- 1.0)
-#result <- within(result, snr <- log(snr))
-
-result$startAngle <- rep(x[1:length(x)-1], each=1, times=rec_num)
-result$endAngle = rep(x[2:length(x)], each=1, times=rec_num)
-result
-write.table(result, '/home/shamik/Documents/Arjun/result_100.csv', col.names = FALSE)
-
+for (dr in dir()) {
+  y <- read_csv(sprintf("%s/avgsnr.txt", dr), col_names = FALSE)
+  y = y[rn,]
+  
+  #y <- replicate(length(x)-1, runif(rec_num))
+  
+  snr_list <- as.vector(t(y))
+  length(snr_list)
+  result$snr = snr_list
+  result$radius = 0.7
+  result <- within(result, radius[is.nan(snr)] <- 0.0)
+  result <- within(result, snr[is.nan(snr)] <- 0.0)
+  #result <- within(result, snr[snr < 1.0] <- 1.0)
+  #result <- within(result, snr <- log(snr))
+  
+  result$startAngle <- rep(x[1:length(x)-1], each=1, times=rec_num)
+  result$endAngle = rep(x[2:length(x)], each=1, times=rec_num)
+  result
+  write.table(result, sprintf('%s/result.csv', dr), col.names = FALSE)
+}
